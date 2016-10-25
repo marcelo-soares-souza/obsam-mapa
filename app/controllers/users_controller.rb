fc0070031672a7ec0276cf2d5c33bae0c4e4c713
Marcelo Soares Souza <marcelo@riseup.net>
2016-10-25 14:42:06 -0200

    Adicionado Criação de Usuário dentro da Interface de Administração

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,25 @@ class UsersController < ApplicationController
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
   def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
@@ -32,6 +50,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:id, :email, :fullname, :slug)
+      params.require(:user).permit(:id, :email, :fullname, :password, :password_confirmation, :admin, :slug)
     end
 end
